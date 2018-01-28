f4d300d6d54e1f6bdb432081815a0b52a1106626
huaying1988 <huaying1988@gmail.com>
2018-01-28 19:03:38 +0800

    Create config.lua

---
+++ expect/config.lua
@@ -0,0 +1,11 @@
+return {
+	wifiSID = "yourWIFISID",
+	wifiPWD = "yourWIFIpassword",
+	postKey = "yourPostKey",
+	postDomain = "yourDomain.com",
+	postURL = "yourURL.do",
+	taskInterval = 5*60*1000,
+	serverPort = 80,
+	connTimeout = 30,
+	dht11pin = 4 --3-GPIO0,4-GPIO2
+}
